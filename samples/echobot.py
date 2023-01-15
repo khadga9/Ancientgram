0866f80804533fa45c7789ac9cd4e744ba7b4104
khadga9 <120645804+khadga9@users.noreply.github.com>
2023-01-15 12:20:36 +0100

    Update echobot.py

--- samples/echobot.py
+++ samples/echobot.py
@@ -3,6 +3,8 @@ from ancientgram import ancientgram
 bot = ancientgram.Ancientgram("123456:your_token")
 
 def handle_update(update):
-    print(update)
-    bot.sendMessage(chat_id=update['message']['from']['id'], text=update['message']['text'])
+    try:
+        bot.sendMessage(chat_id=update['message']['from']['id'], text=update['message']['text'])
+    except:
+        pass
 bot.loop(handle_update)
