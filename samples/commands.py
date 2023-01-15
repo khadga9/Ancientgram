8cc5cb5fe25e559f90fb95ceb83072dc136ed6f3
khadga9 <120645804+khadga9@users.noreply.github.com>
2023-01-15 12:19:38 +0100

    Update commands.py

--- samples/commands.py
+++ samples/commands.py
@@ -3,12 +3,15 @@ from ancientgram import ancientgram
 bot = ancientgram.Ancientgram("123456:your_token")
 
 def handle_update(update):
-    message = update['message']['text']
+    try:
+        message = update['message']['text']
 
-    if message == "/start":
-        bot.sendMessage(chat_id=update['message']['from']['id'], text="Hi, thanks for starting me!")
-    if message == "/who":
-        bot.sendMessage(chat_id=update['message']['from']['id'], text="Hi, I'm using Ancientgram!")
-    if message == "/help":
-        bot.sendMessage(chat_id=update['message']['from']['id'], text="Help message, my commands are /help and /who")
+        if message == "/start":
+            bot.sendMessage(chat_id=update['message']['from']['id'], text="Hi, thanks for starting me!")
+        if message == "/who":
+            bot.sendMessage(chat_id=update['message']['from']['id'], text="Hi, I'm using Ancientgram!")
+        if message == "/help":
+            bot.sendMessage(chat_id=update['message']['from']['id'], text="Help message, my commands are /help and /who")
+    except:
+        pass
 bot.loop(handle_update)
